2fa27b2d16b9f59e9639e550e37250c0733d07b9
Varunrathi8432 <varunrathi8432@gmail.com>
2024-03-06 02:37:54 +0530

    Typeconversion

---
+++ Basic/typeconversion.js
@@ -0,0 +1,32 @@
+let score="78"
+
+console.log(typeof(score))
+
+let newscore=Number(score)
+
+console.log(typeof(newscore))
+console.log(newscore)
+
+let num="243vads"
+num=Number(num)
+
+console.log(typeof(num))
+console.log(num)
+
+let check=1;
+check=Boolean(check)
+
+console.log(check)
+
+//Number conersion
+// "78" -> 78
+// "98yuy" -> NaN(not a number)
+
+//Boolean conversion
+// 1->true ; 0->False
+
+let someNum=123
+someNum=String(someNum)
+
+console.log(someNum)
+console.log(typeof someNum)
